c1e554fd50086191448b858f617f73cf9f73f7b0
Sphosh <37329766+Sphosh@users.noreply.github.com>
2019-09-24 15:44:32 +0200

    Add files via upload

---
+++ UserEntity.java
@@ -0,0 +1,80 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="user_table")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	 private String name;
+	 private String sourcePassport; 
+	 private String destinationPassport;
+	 private String destinationBank;
+     private String destinationCountry;
+     private String accountNumber;
+     private double amount;
+     private int transactionCode;
+     
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSourcePassport() {
+		return sourcePassport;
+	}
+	public void setSourcePassport(String sourcePassport) {
+		this.sourcePassport = sourcePassport;
+	}
+	public String getDestinationPassport() {
+		return destinationPassport;
+	}
+	public void setDestinationPassport(String destinationPassport) {
+		this.destinationPassport = destinationPassport;
+	}
+	public String getDestinationBank() {
+		return destinationBank;
+	}
+	public void setDestinationBank(String destinationBank) {
+		this.destinationBank = destinationBank;
+	}
+	public String getDestinationCountry() {
+		return destinationCountry;
+	}
+	public void setDestinationCountry(String destinationCountry) {
+		this.destinationCountry = destinationCountry;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getTransactionCode() {
+		return transactionCode;
+	}
+	public void setTransactionCode(int transactionCode) {
+		this.transactionCode = transactionCode;
+	}
+     
+     
+}
